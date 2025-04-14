a5b5d1d7a5155e44e84670d8be8b077a926aabbc
Aryakumar Jaiswal <86922289+Aryakumarjaiswal@users.noreply.github.com>
2025-04-14 12:49:57 +0530

    Update text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -6,8 +6,8 @@ engine = pyttsx3.init()
 # Sample list of statements
 statements = [
 "आज की ताजा खबर, हिंदी न्यूज़ 24 अगस्त 2024",
-"Hello Aryakumar!!You are good person.",
-"Soon you will meet your future wife"
+"Hello Aryakumar!!.",
+
 ]
 i=0
 # Loop through each statement and speak it
